6a615bff090bc7d36ee16a051657279a69fa24c0
Brady Vercher <brady@blazersix.com>
2015-10-14 17:23:14 -0700

    Simplify the makepot Grunt config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,16 +26,8 @@ module.exports = function(grunt) {
 				options: {
 					mainFile: 'gravity-forms-iframe.php',
 					potHeaders: {
-						'report-msgid-bugs-to': 'http://wordpress.org/support/plugin/gravity-forms-iframe',
-						'language': 'en',
-						'plural-forms': 'nplurals=2; plural=(n != 1);',
-						'x-poedit-basepath': '../',
-						'x-poedit-bookmarks': '',
-						'x-poedit-country': 'United States',
-						'x-poedit-keywordslist': '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;',
-						'x-poedit-searchpath-0': '.',
-						'x-poedit-sourcecharset': 'utf-8',
-						'x-textdomain-support': 'yes'
+						'poedit': true,
+						'report-msgid-bugs-to': 'https://github.com/bradyvercher/gravity-forms-iframe/issues'
 					},
 					type: 'wp-plugin',
 					updateTimestamp: false
